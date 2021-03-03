3168421b4ad0208ee6409f4727a4e06e02e9a8a9
AL33H <aleff.remberto@hotmail.com>
2021-03-03 11:28:25 -0300

    entities

--- src/main/java/Model/DAO/AgendamentoDAO.java
+++ src/main/java/Model/DAO/AgendamentoDAO.java
@@ -1,5 +1,57 @@
 package Model.DAO;
 
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import Model.Entities.Agendamento;
+import Model.Entities.Funcionario;
+
 public class AgendamentoDAO {
 
+	private static AgendamentoDAO instance;
+	protected EntityManager entityManager;
+	
+	public static AgendamentoDAO getInstance() {
+		if (instance == null) {
+			instance = new AgendamentoDAO();
+		}
+
+		return instance;
+	}
+	
+	private AgendamentoDAO() {
+        entityManager = getEntityManager();
+      }
+
+	private EntityManager getEntityManager() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("exemplo-jpa");
+		if (entityManager == null) {
+			entityManager = factory.createEntityManager();
+		}
+
+		return entityManager;
+	}
+	
+	public void inserir(Agendamento agendamento) {
+		entityManager.getTransaction().begin();;
+		entityManager.merge(agendamento);
+		entityManager.getTransaction().commit();
+	}
+	
+	public void remover(Agendamento agendamento) {
+		entityManager.getTransaction().begin();
+		Agendamento agen = entityManager.find(Agendamento.class, agendamento.getId());
+		entityManager.remove(agen);
+		entityManager.getTransaction().commit();
+	}
+	
+	public void listarPorDia(LocalDate data) {}
+	public void listarPorFuncionario(Funcionario funcionario) {}
+	
+	
+	
+	
 }
